5be3f8db07aabe1b5dbb083c11865460de9134c2
Rick Corbit <rcorbit@gmail.com>
2011-08-25 16:51:42 -0400

    added round to test class in gravatar

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -31,7 +31,7 @@ describe UsersController do
     
     it "should have a profile image" do
       get :show, :id => @user
-      response.should have_selector('h1>img', :class => "gravatar")
+      response.should have_selector('h1>img', :class => "gravatar round")
     end
     
     it "should have the right URL" do
